77ec2d4d057953b579b31f218eb6f8af3a5b9825
Aarni Koskela <akx@iki.fi>
2020-12-01 11:07:48 +0200

    Blacken

--- d01p02.py
+++ d01p02.py
@@ -1,8 +1,8 @@
 import itertools
 
 with open("d01-input.txt", "r") as f:
-	entries = [int(ent.strip()) for ent in f]
-	for a, b, c in itertools.combinations(entries, 3):
-		if a + b + c == 2020:
-			print(a * b * c)
-			break
+    entries = [int(ent.strip()) for ent in f]
+    for a, b, c in itertools.combinations(entries, 3):
+        if a + b + c == 2020:
+            print(a * b * c)
+            break
