74b24c0bed96a28332a9642aa74ec849d8d7becd
Aarni Koskela <akx@iki.fi>
2020-12-01 10:52:13 +0200

    Make d01p02 elegant

--- d01p02.py
+++ d01p02.py
@@ -1,8 +1,8 @@
+import itertools
+
 with open("d01-input.txt", "r") as f:
 	entries = [int(ent.strip()) for ent in f]
-	for i, a in enumerate(entries):
-		for j, b in enumerate(entries[:i]):
-			for c in entries[:(i + j)]:
-				if a + b + c == 2020:
-					print(a * b * c)
-					break
+	for a, b, c in itertools.combinations(entries, 3):
+		if a + b + c == 2020:
+			print(a * b * c)
+			break
