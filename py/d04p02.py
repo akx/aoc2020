53ef77c2368e47d07e156490d234e922c1758a4e
Aarni Koskela <akx@iki.fi>
2020-12-04 13:51:07 +0200

    De-regex

--- py/d04p02.py
+++ py/d04p02.py
@@ -1,4 +1,4 @@
-import re
+from string import hexdigits
 
 from d04lib import read_d04, good_ecl, required_fields
 
@@ -16,7 +16,7 @@ validators = {
     "iyr": lambda x: 2010 <= int(x) <= 2020,
     "eyr": lambda x: 2020 <= int(x) <= 2030,
     "hgt": validate_hgt,
-    "hcl": lambda x: bool(re.match("^#[0-9a-f]{6}$", x)),
+    "hcl": lambda x: len(x) == 7 and x[0] == "#" and all(c in hexdigits for c in x[1:]),
     "ecl": lambda x: x in good_ecl,
     "pid": lambda x: len(x) == 9 and x.isdigit(),
     "cid": lambda x: True,
